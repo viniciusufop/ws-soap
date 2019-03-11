ff90bdbf57b2dd1b4119522f37922f624e7713b8
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-10 22:01:52 -0300

    classes criadas pelo mapeamento e configuração do web service

--- src/main/java/br/com/vfs/wssoap/dto/Country.java
+++ src/main/java/br/com/vfs/wssoap/dto/Country.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.03.10 às 09:46:03 PM BRT 
+// Gerado em: 2019.03.10 às 09:58:36 PM BRT 
 //
 
 
