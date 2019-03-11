88daebae2be7f212998af3ea624a02cfd1de36d9
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-10 21:41:21 -0300

    commit inicial

---
+++ src/main/java/br/com/vfs/wssoap/WsSoapApplication.java
@@ -0,0 +1,13 @@
+package br.com.vfs.wssoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vfs/wssoap/WsSoapApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vfs.wssoap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsSoapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
